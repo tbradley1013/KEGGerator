f6f345e721389bd22fd92d12d3b1b374501c0e84
Tyler Bradley <tyler.bradley@phila.gov>
2018-06-18 09:33:08 -0400

    added functions to find related modules, enzymes and orthologies by pathway. And finding related modules from enzymes and ortholiges

---
+++ R/get-module.R
@@ -0,0 +1,74 @@
+#' Query all modules related to each enzyme in a dataset
+#'
+#' @param data a tibble that is output from get_pathway_enzymes()
+#'
+#' @importFrom magrittr %>%
+#'
+#' @export
+get_enzyme_module <- function(data){
+  if (!"tbl_df" %in% class(data)) stop("data must be of class tbl_df")
+
+  if (!"enzyme_id" %in% colnames(data)) stop("data must contian a column named 'enzyme_id'")
+
+  output <- data %>%
+    dplyr::mutate(
+      module = purrr::map(enzyme_id, ~{
+        mods <- KEGGREST::keggLink("module", .x)
+
+        if (length(mods) > 0) {
+          output <- tibble::tibble(
+            module_id = mods
+          ) %>%
+            dplyr::left_join(kegg_modules, by = "module_id")
+        } else {
+          output <- tibble::tibble(
+            module_id = "None",
+            module = "None"
+          )
+        }
+
+        return(output)
+      })
+    ) %>%
+    tidyr::unnest(module)
+
+  return(output)
+}
+
+
+#' Query all modules related to each orthology in a dataset
+#'
+#' @param data a tabble that is output from get_pathway_orthologies()
+#'
+#' @importFrom magrittr %>%
+#'
+#' @export
+get_orthology_module <- function(data){
+  if (!"tbl_df" %in% class(data)) stop("data must be of class tbl_df")
+
+  if (!"orthology_id" %in% colnames(data)) stop("data must contain a column named 'orthology_id'")
+
+  output <- data %>%
+    dplyr::mutate(
+      module = purrr::map(orthology_id, ~{
+        mods <- KEGGREST::keggLink("module", .x)
+
+        if (length(mods) > 0){
+          output <- tibble::tibble(
+            module_id = mods
+          ) %>%
+            dplyr::left_join(kegg_modules, by = "module_id")
+        } else {
+          output <- tibble::tibble(
+            module_id = "None",
+            module = "None"
+          )
+        }
+
+        return(output)
+      })
+    ) %>%
+    tidyr::unnest(module)
+
+  return(output)
+}
---
+++ R/pathway-links.R
@@ -0,0 +1,93 @@
+#' Query all modules associated with a given pathway
+#'
+#' @param pathway_name a character string specifying a certain pathway. Regular
+#' expressions are used to match the pathway so full pathway names do not have
+#' to be given
+#'
+#' @importFrom magrittr %>%
+#' @details the value passed to pathway_name is searched against the pathway
+#' column in the kegg_pathways dataset
+#'
+#' @export
+get_pathway_modules <- function(pathway_name) {
+  pathway <- kegg_pathways %>%
+    dplyr::filter(stringr::str_detect(pathway, pathway_name)) %>%
+    dplyr::mutate(
+      module = purrr::map(pathway_id, ~{
+        mods <- KEGGREST::keggLink("module", .x)
+
+        output <- tibble::tibble(
+          module_id = mods
+        ) %>%
+          dplyr::left_join(kegg_modules, by = "module_id")
+
+        return(output)
+      })
+    ) %>%
+    tidyr::unnest(module)
+
+  return(pathway)
+}
+
+
+#' Query all enzymes associated with a given pathway
+#'
+#' @param pathway_name a character string specifying a certain pathway. Regular
+#' expressions are used to match the pathway so full pathway names do not have
+#' to be given.
+#'
+#' @importFrom magrittr %>%
+#' @details the value passed to pathway_name is searched against the pathway
+#' column in the kegg_pathways dataset
+#'
+#' @export
+get_pathway_enzymes <- function(pathway_name) {
+  pathway <- kegg_pathways %>%
+    dplyr::filter(stringr::str_detect(pathway, pathway_name)) %>%
+    dplyr::mutate(
+      enzyme = purrr::map(pathway_id, ~{
+        enzymes <- KEGGREST::keggLink("enzyme", .x)
+
+        output <- tibble::tibble(
+          enzyme_id = enzymes
+        ) %>%
+          dplyr::left_join(kegg_enzymes, by = "enzyme_id")
+
+        return(output)
+      })
+    ) %>%
+    tidyr::unnest(enzyme)
+
+  return(pathway)
+}
+
+#' Query all orthologies associated with a given pathway
+#'
+#' @param pathway_name a character string specifying a certain pathway. Regular
+#' expressions are used to match the pathway so full pathway names do not have
+#' to be given.
+#'
+#' @importFrom magrittr %>%
+#' @details the value passed to pathway_name is searched against the pathway
+#' column in the kegg_pathways dataset
+#'
+#' @export
+get_pathway_orthologies <- function(pathway_name){
+  pathway <- kegg_pathways %>%
+    dplyr::filter(stringr::str_detect(pathway, pathway_name)) %>%
+    dplyr::mutate(
+      orthology = purrr::map(pathway_id, ~{
+        orth <- KEGGREST::keggLink("orthology", .x)
+
+        output <- tibble::tibble(
+          orthology_id = orth
+        ) %>%
+          dplyr::left_join(kegg_orthologies, by = "orthology_id")
+
+        return(output)
+      })
+    ) %>%
+    tidyr::unnest(orthology)
+
+  return(pathway)
+}
