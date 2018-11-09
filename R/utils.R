7674ddb7ecdbd55340c572e4d52e7e5ec89ea56c
Tyler Bradley <tyler.bradley@phila.gov>
2018-11-09 11:15:05 -0500

    added a safe function for keggFind

--- R/utils.R
+++ R/utils.R
@@ -7,3 +7,6 @@ kegg_get_safe <- purrr::possibly(KEGGREST::keggGet, otherwise = list(NA_characte
 
 # safe keggLink function
 kegg_link_safe <- purrr::possibly(KEGGREST::keggLink, otherwise = NA_character_)
+
+# safe keggFind function
+kegg_find_safe <- purrr::possibly(KEGGREST::keggFind, otherwise = NA_character_)
