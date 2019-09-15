546570bf3ed55d7379bb97584f337c7af8e93e47
Tyler Bradley <tyler.bradley@phila.gov>
2019-09-15 08:36:11 -0400

    added strict argument

--- R/tibble-genomes.R
+++ R/tibble-genomes.R
@@ -2,9 +2,11 @@
 #'
 #' @param data either a phyloseq object or the output of tibble_tax
 #' @param drop_taxa logical; should the taxonomy be removed from the output
+#' @param strict logical; if set to TRUE, it will not keep any taxa that have
+#' not been classified to the species level.
 #'
 #' @export
-genomes_tibble <- function(data, drop_taxa = TRUE){
+genomes_tibble <- function(data, drop_taxa = TRUE, strict = FALSE){
   if (any(class(data) == "phyloseq")) data <- tax_tibble(data)
 
   if (!"tbl_df" %in% class(data)) stop("data must be either of class tbl_df or phyloseq")
@@ -13,6 +15,13 @@ genomes_tibble <- function(data, drop_taxa = TRUE){
     dplyr::mutate_if(is.factor, as.character) %>%
     dplyr::filter(!is.na(Genus)) %>%
     dplyr::mutate(Species = dplyr::if_else(is.na(Species), "", Species))
+
+  # removing the taxa that do not have any species assigned
+  if (strict){
+    taxa <- dplyr::filter(taxa, Species != "")
+  }
+
+
 
   max_uncertainty <- taxa$Species %>%
     stringr::str_count("\\/") %>%
