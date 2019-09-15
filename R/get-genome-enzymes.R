7e667b8ac33cf8ca4725a3d89e4c5ddd9d367c76
Tyler Bradley <tyler.bradley@phila.gov>
2019-09-15 08:57:47 -0400

    added option to provide own kegg enzyme dataset

--- R/get-genome-enzymes.R
+++ R/get-genome-enzymes.R
@@ -2,6 +2,8 @@
 #'
 #' @param data a tibble with genomes and genome_id or the output from `get_genome_id()`
 #' @param pathway_enzymes a vector of pathway enzymes
+#' @param kegg_enzyme a kegg_tbl that has the columns enzyme and enzyme_id. This
+#' can be generated using the get_kegg_enzyme() function.
 #'
 #' @details if `pathway_enzymes` is NULL (default) than all enzymes for each genome
 #' will be returned. If a vector of enzyme_ids is provided than only the enzymes related
@@ -10,9 +12,17 @@
 #' that are related with the pathway of interest.
 #'
 #' @export
-get_genome_enzymes <- function(data, pathway_enzymes = NULL){
+get_genome_enzymes <- function(data, pathway_enzymes = NULL, kegg_enzyme = NULL){
   if (!"tbl_df" %in% class(data)) stop("data must be of class 'tbl_df'")
   if (!"genome_id" %in% colnames(data)) stop("data must have column named 'genome_id'")
+
+  if (is.null(kegg_enzyme)){
+    kegg_enzyme <- KEGGerator::kegg_enzymes
+  } else{
+    if (!is_kegg_tbl(kegg_enzyme, "enzyme")){
+      stop("kegg_enzyme must be a kegg_tbl with columns enzyme and enzyme_id", call. = FALSE)
+    }
+  }
 
   output <- data %>%
     dplyr::mutate(
@@ -30,7 +40,7 @@ get_genome_enzymes <- function(data, pathway_enzymes = NULL){
       })
     ) %>%
     tidyr::unnest(enzyme_id) %>%
-    dplyr::left_join(kegg_enzymes, by = "enzyme_id")
+    dplyr::left_join(kegg_enzyme, by = "enzyme_id")
 
   return(output)
 }
