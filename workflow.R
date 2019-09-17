5a35ea8451eda5e5d4c3fd6ca06d54c6be0a0681
Tyler Bradley <tyler.bradley@phila.gov>
2019-09-17 15:30:23 -0400

    added temp workflow script

---
+++ workflow.R
@@ -0,0 +1,22 @@
+#===============================================================================
+# workflow to help test interactively during development.... need to converrt
+# to unit tests
+#
+# Tyler Bradley
+# 2019-09-16
+#===============================================================================
+
+library(phyloseq)
+library(KEGGerator)
+library(tidyverse)
+
+ps <- read_rds("../WWTP_Impact_on_Stream/data/ps_orig.RDS")
+
+ps <- filter_taxa(ps, function(x)sum(x)>100, prune = TRUE)
+
+# gerenate a
+ps_kegg <- as_keggerator(ps)
+
+
+ps_genomes <- genomes_tibble(ps)
+
