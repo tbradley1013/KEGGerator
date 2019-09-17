21f2ed36acf65dd035046dd5bfad350caa2cde4c
Tyler Bradley <tyler.bradley@phila.gov>
2019-09-17 08:14:20 -0400

    convert phyloseq-to-tibble functions to methods

--- R/phyloseq-to-tibbles.R
+++ R/phyloseq-to-tibbles.R
@@ -7,10 +7,15 @@
 #' Convert OTU table to tibble
 #'
 #' @param data a phyloseq object
-#'
 #' @export
-otu_tibble <- function(data) {
-  if (class(data) != "phyloseq") stop("data must be a phyloseq object")
+otu_tibble <- function(data){
+  UseMethod("otu_tibble")
+}
+
+
+#' @export
+otu_tibble.phyloseq <- function(data) {
+  # if (class(data) != "phyloseq") stop("data must be a phyloseq object")
 
   otu <- phyloseq::otu_table(data)
   if (!phyloseq::taxa_are_rows(otu)) {
@@ -19,30 +24,35 @@ otu_tibble <- function(data) {
 
   output <- otu %>%
     as.data.frame() %>%
-    tibble::rownames_to_column("seq") %>%
+    tibble::rownames_to_column("otu") %>%
     tibble::as_tibble()
 
   return(output)
 }
 
-
 #' Convert tax table to tibble
 #'
 #' @param data a phyloseq object
-#'
 #' @export
 tax_tibble <- function(data){
+  UseMethod("tax_tibble")
+}
+
+
+#' @export
+tax_tibble.phyloseq <- function(data){
   if (class(data) != "phyloseq") stop("data must be a phyloseq object")
 
   tax <- phyloseq::tax_table(data)
 
   output <- tax %>%
     as.data.frame() %>%
-    tibble::rownames_to_column("seq") %>%
+    tibble::rownames_to_column("otu") %>%
     tibble::as_tibble()
 
   return(output)
 }
+
 
 #' Convert sam_data to tibble
 #'
@@ -50,6 +60,12 @@ tax_tibble <- function(data){
 #'
 #' @export
 sam_tibble <- function(data){
+  UseMethod("sam_tibble")
+}
+
+
+#' @export
+sam_tibble.phyloseq <- function(data){
   sam_data <- phyloseq::sam_data(data)
 
   output <- sam_data %>%
