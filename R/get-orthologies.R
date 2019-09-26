8cfc998999257655f5a9a6d723c73817450a36bb
Tyler Bradley <tyler.bradley@phila.gov>
2019-09-26 13:49:19 -0400

    add orthology fun

--- R/get-orthologies.R
+++ R/get-orthologies.R
@@ -21,6 +21,67 @@ get_orthologies <- function(data, pathway_orthologies, kegg_orthology, verbose, 
 get_orthologies.orgs_id <- function(orgs_id, pathway_orthologies, kegg_orthology = NULL,
                                     verbose = FALSE, progress = TRUE){
 
+  if (is.null(kegg_orthology)){
+    kegg_orthology <- KEGGerator::kegg_orthologies
+  } else{
+    if (!is_kegg_tbl(kegg_orthology, "enzyme")){
+      stop("kegg_enzyme must be a kegg_tbl with columns enzyme and enzyme_id", call. = FALSE)
+    }
+  }
+
+  if (!is_filtered(orgs_id)){
+    warning("The orgs_id object provided has not been filtered, did you forget to run filter_orgs()?", call. = FALSE)
+  }
+
+  if (progress){
+    p <- dplyr::progress_estimated(nrow(orgs_id), 10)
+  }
+
+  output <- data %>%
+    dplyr::mutate(
+      orthology_id = purrr::map(genome_id, ~{
+        id <- stringr::str_replace(.x, "gn:", "")
+
+        orths <- kegg_link_safe("orthology", id) %>%
+          unique()
+
+        n_hits <- length(orths)
+
+        if (!is.null(pathway_orthologies)) {
+          orths <- orths[orths %in% pathway_orthologies]
+        }
+
+        n_remain <- length(orths)
+
+        if (verbose){
+          if (!is.null(pathway_orthologies)){
+            cat("Linked ", crayon::red(n_hits), " (", crayon::red(n_remain),  " within the pathway specified) orthologies to genome: ", crayon::blue(.x), "\n")
+          } else {
+            cat("Linked ", crayon::red(n_hits), " orthologies to genome: ", crayon::blue(.x), "\n")
+          }
+        }
+
+        if (progress){
+          p$tick()$print()
+        }
+
+        return(orths)
+      })
+    ) %>%
+    tidyr::unnest(orthology_id) %>%
+    dplyr::left_join(kegg_orthology, by = "orthology_id")
+
+  class(output) <- c("kegg_fun", class(output))
+  attr(output, "query") <- "orthologies"
+
+
+  if (progress & verbose){
+    p$stop()
+  }
+
+
+  return(output)
+
 }
 
 get_genome_orthologies <- function(data, pathway_orthologies = NULL, kegg_orthology = NULL){
