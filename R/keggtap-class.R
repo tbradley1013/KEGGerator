91027b3954daa197cc90f04c0a4fac93fe1ae58a
Tyler Bradley <tyler.bradley@phila.gov>
2019-10-03 11:33:46 -0400

    moved error checking to helper function

--- R/keggtap-class.R
+++ R/keggtap-class.R
@@ -22,18 +22,6 @@ keggtap <- function(pathway_name, kegg_enzyme = NULL,
                     kegg_orthology = NULL, kegg_module = NULL,
                     kegg_pathway = NULL, strict = FALSE){
 
-  if (!match_strict){
-    pathway_match <- paste(tolower(pathway), collapse = "|")
-    pathways <- kegg_pathway[stringr::str_detect(tolower(kegg_pathway$pathway), pathway_match), ]
-  } else {
-    pathway_match <- tolower(pathway)
-    pathways <- kegg_pathway[tolower(kegg_pathway$pathway) %in% pathway_match, ]
-  }
-
-  if (nrow(pathways) == 0){
-    stop("There are no pathways that match your search", call. = FALSE)
-  }
-
   pathway_enzymes <- get_pathway_enzymes(pathway_name, kegg_enzyme = kegg_enzyme, kegg_pathway = kegg_pathway, strict = strict)
   pathway_orthology <- get_pathway_orthologies(pathway_name, kegg_orthology = kegg_orthology, kegg_pathway = kegg_pathway, strict = strict)
   pathway_modules <- get_pathway_modules(pathway_name, kegg_module = kegg_module, kegg_pathway = kegg_pathway, strict = strict)
